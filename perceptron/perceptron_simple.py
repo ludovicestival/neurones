e5b3a061d95180223dc74b89c8c325cc673c6456
Ludovic Estival <estivall@3il.fr>
2025-06-21 10:46:36 +0200

    Update perceptron_simple.py

--- perceptron/perceptron_simple.py
+++ perceptron/perceptron_simple.py
@@ -4,10 +4,11 @@ from tqdm import tqdm
 from activation_function import ActivationFunction
 
 class PerceptronSimple:
-    def __init__(self, learning_rate=0.1):
+    def __init__(self, activation_func, learning_rate=0.1, ):
         self.learning_rate = learning_rate
         self.weights = None
         self.bias = None
+        self.activation = activation_func
 
     def fit(self, X, y, max_epochs=100):
         """
@@ -19,30 +20,20 @@ class PerceptronSimple:
         self.weights = np.random.randn(X.shape[1])
         self.bias = 0.0
 
-        activation = ActivationFunction('heaviside')
+        for _ in range(max_epochs):
+            for entry, result_wanted in zip(X, y):
+                weighted_sum = np.dot(entry, self.weights) + self.bias
+                prediction = self.activation.apply(weighted_sum)
 
-        for entry, result_wanted in zip(X, y):
-            weighted_sum = np.dot(entry, self.weights)
-            prediction = activation.apply(weighted_sum + self.bias)
-
-            # Mise à jour des poids et du biais
-            if prediction != result_wanted:
-                self.weights += self.learning_rate * result_wanted * np.sum(entry)
-                self.bias += self.learning_rate * result_wanted
+                # Mise à jour des poids et du biais
+                if prediction != result_wanted:
+                    self.weights += self.learning_rate * result_wanted * entry
+                    self.bias += self.learning_rate * result_wanted
 
     def predict(self, X):
         """Prédit les sorties pour les entrées X"""
-        y_pred = np.zeros(X.shape[0])
-
-        for i in range(X.shape[0]):
-            entry = X[i] # [0, 0] [0, 1] [1, 0] [1, 1]
-
-            # TODO
-
-            
-            y_pred[i] = 0
-
-        return y_pred
+        linear_output = np.dot(X, self.weights) + self.bias
+        return self.activation.apply(linear_output)
 
     def score(self, X, y):
         """Calcule l'accuracy"""
@@ -50,19 +41,30 @@ class PerceptronSimple:
         return np.mean(predictions == y)
     
 if __name__ == '__main__':
+    activation_functions = [
+        ActivationFunction('heaviside'),
+        ActivationFunction('sigmoid'),
+        ActivationFunction('tanh'),
+        ActivationFunction('relu'),
+        ActivationFunction('leaky_relu')
+    ]
+
     # Données pour la fonction AND
     X_and = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y_and = np.array([-1, -1, -1, 1])  # -1 pour False, 1 pour True
 
-    perceptron = PerceptronSimple()
-    perceptron.fit(X_and, y_and)
-    print(perceptron.predict(X_and))
+    for func in activation_functions:
+        perceptron = PerceptronSimple(activation_func=func)
+        perceptron.fit(X_and, y_and)
+        print(perceptron.predict(X_and))
 
+    print('')
 
     # Données pour la fonction OR
     X_or = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y_or = np.array([-1, 1, 1, 1])
 
-    #perceptron = PerceptronSimple()
-    #perceptron.fit(X_or, y_or)
-    # print(perceptron.predict(X_or))
+    for func in activation_functions:
+        perceptron = PerceptronSimple(activation_func=func)
+        perceptron.fit(X_or, y_or)
+        print(perceptron.predict(X_or))
